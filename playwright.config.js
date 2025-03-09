4d12ffa5023559f2703146b1ce72c2962e1976e5
jungsun-eoh <60766360+jungsun-eoh@users.noreply.github.com>
2025-03-08 20:38:28 -0800

    added test report

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     use: {
         headless: false, // Non-headless mode
-        screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
+        screenshot: 'only-on-failure',  // Capture screenshot on failure
+        video: 'retain-on-failure', // Record video on failure
     },
+    reporter: [['html', { outputFolder: 'playwright-report', open: 'on-failure' }]],    // Enable HTML test report
     projects: [
         { name: 'chromium', use: { browserName: 'chromium' } },
         { name: 'firefox', use: { browserName: 'firefox' } },
